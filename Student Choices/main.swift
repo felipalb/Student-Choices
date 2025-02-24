ff8e1b1c4dd1182a9280bbe9f6cb58971d095cc3
Felipe <felipealbertosantos@outlook.com>
2025-02-24 12:01:12 -0300

    changes main 2

--- Student Choices/main.swift
+++ Student Choices/main.swift
@@ -9,10 +9,14 @@ import Foundation
 
 print ("Teste input")
 var Naluno : String = ""
+// declarei variavel vazia para guardar valor de input
 if let input = readLine() {
     Naluno = input
     print("Olá, \(Naluno)")
 }
+// if para ler a linha (readLine), guardando dentro da var nomeada Naluno
+// retorna depois olá + nome do aluno "\(variavelNome)"
+
 
 
 
