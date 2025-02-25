fc9381a2e271e6bb4b3039203785356867b30a5a
Felipe <felipealbertosantos@outlook.com>
2025-02-25 09:36:24 -0300

    beginning code

--- Student Choices/main.swift
+++ Student Choices/main.swift
@@ -7,12 +7,50 @@
 
 import Foundation
 
-print ("Teste input")
-var Naluno : String = ""
+print ("Bem vindo ao Student Choices!! \nCrie seu personagem!")
+var NameStudent : String = ""
+var Gender : String? = nil
+// declaro como string opcional armazena entrada tipo string e tipo nil
+var auth = false
+var Age : Int = 0
+var University : String = ""
 // declarei variavel vazia para guardar valor de input
+// nome, genero, idade, universidade
+print("Insira o nome: \n")
 if let input = readLine() {
-    Naluno = input
-    print("Olá, \(Naluno)")
+    NameStudent = input
+    print("Olá, \(NameStudent)")
+}
+while !auth {
+    
+print("Selecione o gênero: \n1. Masculino\n2. Feminino\n3. Outro")
+
+if let input = readLine(), let choice = Int(input) {
+    
+    switch choice {
+    case 1:
+        Gender = "Masculino"
+        auth = true
+    case 2:
+        Gender = "Feminino\n"
+        auth = true
+    case 3:
+        Gender = "outro \n"
+        auth = true
+    default:
+        print("opcao invalida")
+    }
+    
+}
+    
+    else{
+        print("teste")
+    }
+    
+}
+
+if let SelectedGen = Gender {
+    print("voce é do genero: \(SelectedGen)")
 }
 // if para ler a linha (readLine), guardando dentro da var nomeada Naluno
 // retorna depois olá + nome do aluno "\(variavelNome)"
