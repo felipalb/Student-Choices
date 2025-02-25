3ec5836ac1855d41ec6b056078a70adb938490ed
Felipe <felipealbertosantos@outlook.com>
2025-02-25 10:56:45 -0300

    code maintenance

--- Student Choices/main.swift
+++ Student Choices/main.swift
@@ -12,47 +12,62 @@ var NameStudent : String = ""
 var Gender : String? = nil
 // declaro como string opcional armazena entrada tipo string e tipo nil
 var auth = false
+// variavel de autenticacao do loop
 var Age : Int = 0
 var University : String = ""
 // declarei variavel vazia para guardar valor de input
 // nome, genero, idade, universidade
-print("Insira o nome: \n")
-if let input = readLine() {
-    NameStudent = input
-    print("Olá, \(NameStudent)")
+while NameStudent.isEmpty{
+// continua executando enquanto o nome for uma string vazia
+    print("Insira o nome: \n")
+        if let input = readLine(), !input.isEmpty {
+            // verifica se a entrada não está vazia
+            NameStudent = input
+            print("Olá, \(NameStudent)")
+        }
+    else{
+        print("informe um nome para seu personagem")
+    }
 }
 while !auth {
+// garante que o loop continue até que uma escolha válida seja feita
     
 print("Selecione o gênero: \n1. Masculino\n2. Feminino\n3. Outro")
 
 if let input = readLine(), let choice = Int(input) {
-    
+// crio variavel provisoria choice, para comparar choice com os casos
     switch choice {
-    case 1:
-        Gender = "Masculino"
-        auth = true
-    case 2:
-        Gender = "Feminino\n"
-        auth = true
-    case 3:
-        Gender = "outro \n"
-        auth = true
-    default:
-        print("opcao invalida")
+        case 1:
+            Gender = "Masculino"
+            //armazeno a escolha string na gender
+            auth = true
+            //autentico como true a var auth para validar escolha
+        case 2:
+            Gender = "Feminino"
+            auth = true
+        case 3:
+            Gender = "Outro"
+            auth = true
+        default:
+            // se escolha não for 1,2,3 executa aqui
+            print("opção invalida")
     }
-    
 }
-    
-    else{
-        print("teste")
+    // verifico se o valor da entrada é numero int ou não default
+    else {
+        print("Insira um valor válido.")
     }
-    
 }
 
-if let SelectedGen = Gender {
-    print("voce é do genero: \(SelectedGen)")
+// if para mudar de outro para neutro
+if Gender == "Outro" {
+    Gender = "Neutro"
 }
-// if para ler a linha (readLine), guardando dentro da var nomeada Naluno
+// chamo uma variavel provisoria para receber o valor de gender
+if let SelectedGen = Gender {
+    print("voce é do genero: \(SelectedGen)\n")
+}
+// if para ler a linha (readLine), guardando dentro da var
 // retorna depois olá + nome do aluno "\(variavelNome)"
 
 
